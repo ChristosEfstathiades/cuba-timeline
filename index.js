4410ab9bd1f84eab9f80e68eb22794e8fddaa9e8
Chris Efstathiades <chrise3012@icloud.com>
2019-04-29 22:21:53 +0100

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert("hello");
+ScrollReveal().reveal('.container', {
+
+  duration: 1000,
+  viewFactor: 0.6,
+  distance: '350px'
+});
+
+ScrollReveal().reveal('.right', {
+  origin:'right'
+})
+
+ScrollReveal().reveal('.left', {
+  origin:'left'
+})
